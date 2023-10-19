8a006c6f7a571a262bb5fe95e242c996d21598c3
RedsonBr140 <redson@riseup.net>
2023-10-19 12:22:08 -0300

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/GridMaster/",
+    base: "https://redsonbr140.github.io/GridMaster/",
     server: {
         port: 3000,
         host: "0.0.0.0",
